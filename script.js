7f3d667383cd009b8971f0309edac8d5563ba1f8
Ezekiel Searles <zekedsearles@gmail.com>
2024-01-19 12:24:26 -0800

    yup

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const element = React.createElement(
+    'h1',
+    {
+      className: 'title',
+      onClick: () => {console.log('clicked');}
+    },
+    'Hello World!'
+);
 
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(element)
