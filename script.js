944efe12e3c816a80c006301772dab31adb4195a
Ezekiel Searles <zekedsearles@gmail.com>
2024-01-19 15:59:53 -0800

    finished first assignment

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-const element = React.createElement(
-    'h1',
-    {
-      className: 'title',
-      onClick: () => {console.log('clicked');}
-    },
-    'Hello World!'
-);
+const element = <h1>Hello World!</h1>
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 root.render(element)
+
+const container2 = document.getElementById('root2');
+const root2 = ReactDOM.createRoot(container2);
+root2.render(element);
+
+document.querySelectorAll('.button').forEach(domContainer => {
+  const messageId = parseInt(domContainer.dataset.messageId);
+
+  const root = ReactDOM.createRoot(domContainer);
+
+  root.render(
+    React.createElement(
+      'button',
+      {
+        onClick: () => { console.log(`liked message: ${messageId}`); }
+      },
+      'like'
+    )
+  )
+});
